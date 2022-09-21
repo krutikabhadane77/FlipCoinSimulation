039e6339b53f6895de94272cf552930cd27f2f65
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-09-21 23:12:45 +0530

    Added in: Flipping Coin either of them have won 21 times with Difference of Min 2 points achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,12 @@
 target_count=21
+min_diff=2
 heads_count=0
 tails_count=0
 flips_count=0
 while(( 1 ))
 do
     (( flips_count++ ))
-    echo -n "Flip-$flips_count is "
+    echo -n  $flips_count 
     toss=$(( RANDOM % 2 ))
     if(( toss == 0 ))
     then
@@ -15,19 +16,18 @@ do
         echo "Tails"
         (( tails_count++ ))
     fi
-    if(( heads_count == target_count || tails_count == target_count ))
+    
+    diff_bt_hc_tc=$(( heads_count - tails_count ))
+    if(( heads_count == target_count && diff_bt_hc_tc >= min_diff ))
     then
+        echo "Heads won $diff_bt_hc_tc points"
         break
-    fi 
+    elif(( tails_count == target_count && ${diff_bt_hc_tc#-} >= min_diff ))
+    then
+         echo "Tails won ${diff_bt_hc_tc#-} points"
+         break
+    fi
+    
 done
-echo "Heads count is $heads_count" 
+echo "Heads count is $heads_count"
 echo "Tails Count is $tails_count"
-if(( heads_count > tails_count ))
-then
-    echo "Heads won by $(( heads_count - tails_count ))"
-elif(( tails_count > heads_count ))
-then
-    echo "Tails won by $(( tails_count - heads_count ))"
-else    
-    echo "It is tie"
-fi
